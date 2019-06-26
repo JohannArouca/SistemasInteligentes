81dc6c4d3e393455ebfaf4a150f4cc96583a99e5
Johan <aroucajohann@gmail.com>
2019-06-26 17:21:55 -0300

    first commit

---
+++ src/HelloWorldAG/AGhelloWorld.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloWorldAG;
+
+public class AGhelloWorld {
+
+    public static void main(String[] args) {
+
+        //Define a solução
+        Algoritimo.setSolucao("jao e resende");
+        //Define os caracteres existentes
+        Algoritimo.setCaracteres("!,.:;?áÁãÃâÂõÕôÔóÓéêíÉÊQWERTYUIOPASDFGHJKLÇZXCVBNMqwertyuiopasdfghjklçzxcvbnm1234567890 ");
+        //taxa de crossover de 60%
+        Algoritimo.setTaxaDeCrossover(0.6);
+        //taxa de mutação de 3%
+        Algoritimo.setTaxaDeMutacao(0.3);
+        //elitismo
+        boolean eltismo = true;
+        //tamanho da população 100
+        int tamPop = 100;
+        //numero máximo de gerações
+        int numMaxGeracoes = 15000;
+
+        //define o número de genes do indivíduo baseado na solução
+        int numGenes = Algoritimo.getSolucao().length();
+
+        //cria a primeira população aleatérioa
+        Populacao populacao = new Populacao(numGenes, tamPop);
+
+        boolean temSolucao = false;
+        int geracao = 0;
+
+        System.out.println("Iniciando... Aptidão da solução: "+Algoritimo.getSolucao().length());
+        
+        //loop até o critério de parada
+        while (!temSolucao && geracao < numMaxGeracoes) {
+            geracao++;
+
+            //cria nova populacao
+            populacao = Algoritimo.novaGeracao(populacao, eltismo);
+
+            System.out.println("Geração " + geracao + " | Aptidão: " + populacao.getIndivduo(0).getAptidao() + " | Melhor: " + populacao.getIndivduo(0).getGenes());
+            
+            //verifica se tem a solucao
+            temSolucao = populacao.temSolocao(Algoritimo.getSolucao());
+        }
+
+        if (geracao == numMaxGeracoes) {
+            System.out.println("Número Maximo de Gerações | " + populacao.getIndivduo(0).getGenes() + " " + populacao.getIndivduo(0).getAptidao());
+        }
+
+        if (temSolucao) {
+            System.out.println("Encontrado resultado na geração " + geracao + " | " + populacao.getIndivduo(0).getGenes() + " (Aptidão: " + populacao.getIndivduo(0).getAptidao() + ")");
+        }
+    }
+}
+
